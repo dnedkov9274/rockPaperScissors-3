0e677b0b0b3c045a019c9a8124424d920a445f06
Dennis Nedkov <Dnedkov9274@jh-mba-jacks.local>
2019-02-11 13:26:44 -0600

    what I've worked on so far for Rock Paper Scissors

--- rockPaperScissors/ViewController.swift
+++ rockPaperScissors/ViewController.swift
@@ -9,12 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var opponentImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    
+    
+    
+    @IBAction func rock(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func paper(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func scissors(_ sender: UIButton) {
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        createAlert(title:"Winner", message: "Congratulations, you won this game of rock, paper, scissors!")
+    }
+    func createAlert(title:String, message:String)
+    {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction (title: "ok", style: .default
+            , handler: {(action) in
+                alert.dismiss(animated: true, completion: nil)
+                
+        }))
+        
+        
+    }
+    
+    
+    
+    
 }
+
+
+
+
 
