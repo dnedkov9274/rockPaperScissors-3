cce6c7157f65a3d2f1437c6196f1938858997a9f
mobileapps <mobileapps@jh-mba158-21.local>
2019-02-14 14:16:15 -0600

    fixed SIGBART error with selecting images and added in imageArray.randomelemen() to all buttons

--- rockPaperScissors/ViewController.swift
+++ rockPaperScissors/ViewController.swift
@@ -14,56 +14,55 @@ class ViewController: UIViewController {
     @IBOutlet weak var myImage: UIImageView!
     @IBOutlet weak var opponentImage: UIImageView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
     
     let imageArray: [UIImage] = [
-        
         UIImage(named: "rockImage")!,
         UIImage(named: "paperImage")!,
         UIImage(named: "scissorsImage")!
     ]
     
-    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
     
     @IBAction func rock(_ sender: UIButton) {
         myImage.image = UIImage(named: "rockImage")
+        imageArray.randomElement()
         
     }
     
     @IBAction func paper(_ sender: UIButton) {
         myImage.image = UIImage(named: "paperImage")
-        
+        imageArray.randomElement()
     }
     
     @IBAction func scissors(_ sender: UIButton) {
         myImage.image = UIImage(named: "scissorsImage")
-        
+        imageArray.randomElement()
     }
     
     
-//    override func viewDidAppear(_ animated: Bool) {
-//        createAlert(title:"Winner", message: "Congratulations, you won this game of rock, paper, scissors!")
-//    }
-//    func createAlert(title:String, message:String)
-//    {
-//        let winAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-//        winAlert.addAction(UIAlertAction (title: "ok", style: .default
-//            , handler: {(action) in
-//                winAlert.dismiss(animated: true, completion: nil)
-//                
-//                let loseAlert = UIAlertController(title: "Sorry!", message: "You lost, better luck next time!", preferredStyle: .alert)
-//                
-//                let tieAlert = UIAlertController(title: "Nice Try!", message: "You tied, try again!", preferredStyle: .alert)
-//                
-//                
-//                
-//                
-//        }))
-//        
-//        
-//    }
+    //    override func viewDidAppear(_ animated: Bool) {
+    //        createAlert(title:"Winner", message: "Congratulations, you won this game of rock, paper, scissors!")
+    //    }
+    //    func createAlert(title:String, message:String)
+    //    {
+    //        let winAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+    //        winAlert.addAction(UIAlertAction (title: "ok", style: .default
+    //            , handler: {(action) in
+    //                winAlert.dismiss(animated: true, completion: nil)
+    //
+    //                let loseAlert = UIAlertController(title: "Sorry!", message: "You lost, better luck next time!", preferredStyle: .alert)
+    //
+    //                let tieAlert = UIAlertController(title: "Nice Try!", message: "You tied, try again!", preferredStyle: .alert)
+    //
+    //
+    //
+    //
+    //        }))
+    //
+    //
+    //    }
     
     
     
